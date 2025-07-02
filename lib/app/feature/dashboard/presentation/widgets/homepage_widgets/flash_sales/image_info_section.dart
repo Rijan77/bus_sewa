1862c27b890f22241811d7a0c85930636ea3db8c
Rijan77 <rijanacharya73@gmail.com>
2025-07-02 17:19:14 +0545

    Sucessfully Featch Data from Api using Cubit

--- lib/app/feature/dashboard/presentation/widgets/homepage_widgets/flash_sales/image_info_section.dart
+++ lib/app/feature/dashboard/presentation/widgets/homepage_widgets/flash_sales/image_info_section.dart
@@ -1,49 +1,103 @@
 import 'package:bus_sewa/app/core/common_widgets/text_style_widget.dart';
+import 'package:bus_sewa/app/core/utils/api_status.dart';
+import 'package:bus_sewa/app/feature/dashboard/presentation/blocs/flash_sales_data_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../blocs/flash_sales_data_state.dart';
 
-class ImageInfoSection extends StatelessWidget {
+class ImageInfoSection extends StatefulWidget {
   const ImageInfoSection({super.key});
+
+  @override
+  State<ImageInfoSection> createState() => _ImageInfoSectionState();
+}
+
+class _ImageInfoSectionState extends State<ImageInfoSection> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    context.read<FlashSalesDataCubit>().fetchData();
+  }
 
   @override
   Widget build(BuildContext context) {
 
+    return BlocBuilder<FlashSalesDataCubit, FlashSaleDataState>(builder: (context, state){
+       if(state.status == ApiStatus.initial || state.status == ApiStatus.loading){
+         return const Center(
+           child: CircularProgressIndicator(color: Colors.blueGrey,),
+         );
+       } else if (state.status == ApiStatus.failure){
+         return Center(child: Text(state.error?? "Something went wrong"));
+       } else if(state.status == ApiStatus.success && state.flashSales!= null){
+         return Padding(padding: EdgeInsets.all(10.0),
+           child: ClipRRect(
+             borderRadius: BorderRadius.circular(10.0),
+             child: SingleChildScrollView(
+               scrollDirection: Axis.horizontal,
+
+               child: Row(
+                 children: state.flashSales.map((sale){
+                   return Padding(
+                     padding: const EdgeInsets.only(right: 10),
+                     child: Row(
+                       children: [
+                         _buildRoundedBox(imagePath: sale.imageUrl, timeText: sale.time, title: sale.discount, subTitle: sale.hotelName)
+                       ],
+                     ),
+                   );
+                 }).toList(),
+               ),
+             ),
+           ),
+         );
+       } else{
+         return SizedBox.shrink();
+       }
+
+    });
 
 
 
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              _buildRoundedBox(
-                imagePath: "assets/images/Frame 1171275916 (1).png",
-                timeText: "1H 40M left",
-                title: "Get 20% OFF",
-                subTitle: "Meghauli Hotel",
-              ),
-              const SizedBox(width: 10),
-              _buildRoundedBox(
-                imagePath: "assets/images/Frame 1171275917.png",
-                timeText: "1H 40M left",
-                title: "Get 20% OFF",
-                subTitle: "Meghauli Hotel",
-              ),
-              const SizedBox(width: 10),
-              _buildRoundedBox(
-                imagePath: "assets/images/Frame 1171275918.png",
-                timeText: "1H 40M left",
-                title: "Get 20% OFF",
-                subTitle: "Meghauli Hotel",
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+
+
+
+    // return Padding(
+    //   padding: const EdgeInsets.all(10.0),
+    //   child: ClipRRect(
+    //     borderRadius: BorderRadius.circular(10),
+    //     child: SingleChildScrollView(
+    //       scrollDirection: Axis.horizontal,
+    //       child: Row(
+    //         children: [
+    //           _buildRoundedBox(
+    //             imagePath: "assets/images/Frame 1171275916 (1).png",
+    //             timeText: "1H 40M left",
+    //             title: "Get 20% OFF",
+    //             subTitle: "Meghauli Hotel",
+    //           ),
+    //           const SizedBox(width: 10),
+    //           _buildRoundedBox(
+    //             imagePath: "assets/images/Frame 1171275917.png",
+    //             timeText: "1H 40M left",
+    //             title: "Get 20% OFF",
+    //             subTitle: "Meghauli Hotel",
+    //           ),
+    //           const SizedBox(width: 10),
+    //           _buildRoundedBox(
+    //             imagePath: "assets/images/Frame 1171275918.png",
+    //             timeText: "1H 40M left",
+    //             title: "Get 20% OFF",
+    //             subTitle: "Meghauli Hotel",
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 
   Widget _buildRoundedBox({
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:bus_sewa/app/feature/dashboard/presentation/views/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'app/feature/dashboard/presentation/views/homepage.dart';
 import 'app/feature/dashboard/presentation/blocs/flash_sales_data_cubit.dart';
 
 void main() {
@@ -12,15 +12,24 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [BlocProvider(create: (_) => FlashSalesDataCubit())],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Homepage(),
-      ),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(create: (_) => FlashSalesDataCubit()),
+          ],
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: child,
+          ),
+        );
+      },
+      child: const Homepage(), // 👈 Your main widget goes here
     );
   }
 }
